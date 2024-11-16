45f3c79be30eb1ec0fcbb31f6614617c4306cd2e
salsakamala18 <salsakamala18@gmail.com>
2024-11-16 13:11:06 +0700

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -3,7 +3,7 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-    expected = "Hello W"
+    expected := "Hello W"
     if ret := hello(); ret != expected {
         t.Errorf("hello() is %q want %q", ret, expected)
     }
